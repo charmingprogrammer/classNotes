7e0b7d0fc966b4e93440e008916de0dc47f3542c
Manohar Anand <71115307+charmingprogrammer@users.noreply.github.com>
2021-08-03 11:59:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+//navigation open 
+function  nev__opener() {
+  document.getElementById("nevContainer").style.left = '0';
+  
+}
+//navigation close
+function  nev__closer() {
+  document.getElementById("nevContainer").style.left = '';
+  
+  
+}
---
+++ storyscript.js
@@ -0,0 +1,17 @@
+
+      function on() {
+        document.querySelector("*").style.backgroundColor = "black";
+        document.querySelector("*").style.color = "gray";
+        document.querySelector("#header").style.backgroundColor = "black";
+        document.querySelector("#header").style.color = "gray";
+        
+      }
+      function off() {
+        document.querySelector("*").style.backgroundColor = "";
+        document.querySelector("*").style.color = "";
+        document.querySelector("#header").style.backgroundColor = "";
+        document.querySelector("#header").style.color = "";
+        
+      }
+    
+  
